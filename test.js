5ddb6a8543b1812b780ea838ca382da3abfc8557
tejveer008 <tejveer.sf@gmail.com>
2024-08-01 11:25:06 +0530

    First commit

---
+++ test.js
@@ -0,0 +1,26 @@
+
+// variables
+"use strict"; // treat all JS code as newer version
+
+// const accountId =11221
+// let accountEmail = "Tejveer001@gmail.com"
+// var accountpass = "12345"
+// let accountCity = "meerut"
+
+// console.log(typeof accountId);//number
+// console.log(typeof undefined)//undefined
+// console.log(typeof null) //object
+
+//Conversion Operation
+
+// let score  = 33
+// console.log(typeof score)
+
+// let strToNumber =Number(score)
+// console.log(typeof strToNumber)
+// console.log(strToNumber)
+
+// console.log('1' + 2);
+// console.log('1' + (2 + 2));
+// console.log(1 + 2 + '2' + 2);
+// console.log(1 + 2 + '2');
